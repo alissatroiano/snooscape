704f56a500408121e849230ae73d03821810defd
Alissa Troiano <alissatroianonyc@gmail.com>
2025-03-28 04:21:26 -0400

    update game

--- src/createPost.tsx
+++ src/createPost.tsx
@@ -2,13 +2,13 @@ import { Devvit } from '@devvit/public-api';
 
 // Adds a new menu item to the subreddit allowing to create a new post
 Devvit.addMenuItem({
-  label: 'Create New Devvit Post (with Web View)',
+  label: 'SnooScapes',
   location: 'subreddit',
   onPress: async (_event, context) => {
     const { reddit, ui } = context;
     const subreddit = await reddit.getCurrentSubreddit();
     const post = await reddit.submitPost({
-      title: 'Snooscapes',
+      title: 'SnooScapes',
       subredditName: subreddit.name,
       // The preview appears while the post loads
       preview: (
@@ -17,7 +17,7 @@ Devvit.addMenuItem({
         </vstack>
       ),
     });
-    ui.showToast({ text: 'Created post!' });
+    ui.showToast({ text: 'Initiated new SnooScapes Game!' });
     ui.navigateTo(post);
   },
 });
